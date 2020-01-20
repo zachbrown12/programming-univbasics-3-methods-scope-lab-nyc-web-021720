351f0301caf361a47fc9aab5416143a56fd58c24
Zach Brown <zachmb22@gmail.com>
2020-01-20 15:20:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,17 @@
 
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-
 def mario
-  phrease = "It's-a me, Mario!"
+  phrease = "It's-a me, Mario!\n"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
+  phrase = "It's Dangerous To Go Alone! Take This.\n"
+end
  linkphrase =  "It's Dangerous To Go Alone! Take This.\n"
 end
 
