5591dffde6a1037cdf1789a85e4462a24e3a9628
Zach Brown <zachmb22@gmail.com>
2020-01-20 15:16:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,13 @@
+
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
   puts status
 end
+
+
+
